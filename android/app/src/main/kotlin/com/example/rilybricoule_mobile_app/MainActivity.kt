fa9cb76ff8f54340107827a4d6c2b4efd5f3b714
Marouane Bouglace <bouglacemarouane@gmail.com>
2026-03-05 17:26:04 +0000

    fix: add Facebook key hash generation in MainActivity

--- android/app/src/main/kotlin/com/example/rilybricoule_mobile_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/rilybricoule_mobile_app/MainActivity.kt
@@ -1,5 +1,29 @@
 package com.example.rilybricoule_mobile_app
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.content.pm.PackageManager
+import java.security.MessageDigest
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        try {
+            val info = packageManager.getPackageInfo(
+                packageName,
+                PackageManager.GET_SIGNATURES
+            )
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val keyHash = Base64.encodeToString(md.digest(), Base64.DEFAULT)
+                Log.d("KeyHash", "Key Hash: $keyHash")
+            }
+        } catch (e: Exception) {
+            Log.e("KeyHash", "Error: ${e.message}")
+        }
+    }
+}
